b7f9e1d4c215c50e877d5236d960b59eb369110a
Angga Jiyan <anggajiyanfajar@gmail.com>
2024-06-10 10:30:17 +0700

    payment service enhancment

--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -10,6 +10,16 @@ export class PaymentService {
 
     async createWebhook(body: any) {
         try {
+            const payment = await this.prisma.payment.findUnique({
+                where: {
+                    payment_id: body.order_id
+                }
+            })
+
+            if (!payment) {
+                throw new NotFoundException('Payment with id not found')
+            }
+
             const data = await this.prisma.payment.update({
                 where: {
                     payment_id: body.order_id
@@ -19,7 +29,9 @@ export class PaymentService {
                     status: body.transaction_status
                 }
             })
+            return data
         } catch(error) {
+            console.log(error)
             if (error instanceof NotFoundException) {
                 throw error
             }
